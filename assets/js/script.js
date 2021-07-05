04a18eb1fc7a537ef14b886932517e84370d8619
Homam Atar <homam.attar@gmail.com>
2021-07-05 21:43:41 +0300

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
-let inputBox = document.querySelector('.search-text')
-
-inputBox.addEventListener('input', function() {
-    document.querySelector('.box').style.display = "block";
-});
+// let inputBox = document.querySelector('.search-text')
+//
+// inputBox.addEventListener('input', function() {
+//     document.querySelector('.box').style.display = "block";
+// });
+function ShowMenu() {
+    var x = document.getElementById("profile-menu");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
